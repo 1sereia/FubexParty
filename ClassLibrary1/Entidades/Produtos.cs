96647098bb379f00f4429c294b59cb3e9727a055
leomendesm <leo.mi.me@gmail.com>
2016-05-30 17:49:40 -0300

    xamps

---
+++ ClassLibrary1/Entidades/Produtos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Entidades
+{
+    class Produtos
+    {
+        public int ProdutoID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public Decimal Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
